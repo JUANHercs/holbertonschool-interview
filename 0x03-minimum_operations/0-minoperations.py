72262297e4acdf45e6c6875f4f6d6058c3711804
JUANHercs <juan.hercs@gmail.com>
2022-05-15 23:00:55 -0500

    pep8

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -3,19 +3,20 @@
 the fewest number of operations needed to result in exactly n H
 only ywo operations Copy All (*) and PAste (+)"""
 
+
 def minOperations(n):
 
     if type(n) is not int or n <= 0:
         return 0
 
-    count = 0
-    letter = 1
-    copy = 1
+        count = 0
+        letter = 1
+        copy = 1
 
     while letter < n:
         if n % letter == 0:
             copy = letter
-            count += 1
+        count += 1
         if letter != n:
             letter += copy
             count += 1
