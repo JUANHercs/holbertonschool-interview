caa0536f61093e6006852cf6171b9eba560d3128
JUANHercs <juan.hercs@gmail.com>
2022-05-26 12:55:03 -0500

    done

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - This function checks if
+ * a singly linked list is a palindrome.
+ * @head: The start of the list.
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *aux = *head, *head2 = NULL, *aux2 = NULL;
+
+	while (aux)
+	{
+		add_nodeint(&head2, aux->n);
+		aux = aux->next;
+	}
+	aux2 = head2;
+	aux = *head;
+	while (aux)
+	{
+		if (aux->n != aux2->n)
+		{
+			free_listint(head2);
+			return (0);
+		}
+		aux = aux->next;
+		aux2 = aux2->next;
+	}
+	free_listint(head2);
+	return (1);
+}
+
+/**
+ * add_nodeint - This function adds a new node at the start of a list.
+ * @head: The start of the list.
+ * @n: The value of the node.
+ * Return: The new node.
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new = NULL;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (!*head)
+		*head = new;
+	else
+	{
+		new->next = *head;
+		*head = new;
+	}
+	return (new);
+}
